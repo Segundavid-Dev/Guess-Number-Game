d6d33fe6aeb6951897b29a57bc2b9e6436d846a7
Segun David <segdavid03@gmail.com>
2024-10-21 19:27:51 +0100

    completed guess game first condition

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-"use strict";
+document.querySelector(".check").addEventListener("click", function () {
+  //  console.log(document.querySelector(".guess").value);
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number ❌";
+  }
+});
