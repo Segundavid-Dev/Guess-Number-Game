a5f873238fe6384b94078bf167d0a3704170ed14
Segun David <segdavid03@gmail.com>
2024-10-22 11:39:46 +0100

    Fixed styling issue

--- app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".check").addEventListener("click", function () {
-  //  console.log(document.querySelector(".guess").value);
-  const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
-
-  if (!guess) {
-    document.querySelector(".message").textContent = "No number ❌";
-  }
-});
